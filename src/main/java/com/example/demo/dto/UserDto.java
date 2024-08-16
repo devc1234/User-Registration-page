fedf43ad2c2de674908dd9e3609124a69404b55b
devc1234 <namdevchavan713@gmail.com>
2024-08-16 16:56:32 +0530

    user form updated

---
+++ src/main/java/com/example/demo/dto/UserDto.java
@@ -0,0 +1,29 @@
+package com.example.demo.dto;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@SuppressWarnings("Deprecated")
+public class UserDto
+{
+    private Long id;
+    
+	@NotEmpty
+    private String firstName;
+    @NotEmpty
+    private String lastName;
+    @NotEmpty(message = "Email should not be empty")
+    @Email
+    private String email;
+    @NotEmpty(message = "Password should not be empty")
+    private String password;
+}
